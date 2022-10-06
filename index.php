26161f6540e9f838537eb0e406e4a33228a546b1
Jotawk <Jonathan.tawk@live.fr>
2022-10-06 13:31:47 +0200

    Mise en place du fichier index.php

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Exercice superglobales</title>
 </head>
+
 <body>
 	<p>Mise à jour du fichier</p>
+	<?php var_dump($_GET); ?>
 </body>
+
 </html>
