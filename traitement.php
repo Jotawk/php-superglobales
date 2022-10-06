55e75ae2ab1c4130a0bf32fd184b1a7d1ea1687a
Jotawk <Jonathan.tawk@live.fr>
2022-10-06 14:22:27 +0200

    Enregistrement du produit en session

--- traitement.php
+++ traitement.php
@@ -2,6 +2,18 @@
 	$name = filter_input(INPUT_POST, htmlspecialchars("name"));
 	$price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
 	$qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
+
+	if ($name && $price && $qtt) {
+
+		$product = [
+			"name" => $name,
+			"price" => $price,
+			"qtt" => $qtt,
+			"total" => $price * $qtt
+		];;
+
+		$_SESSION['products'][] = $product;
+	}
 }
 
 header("location:index.php");
