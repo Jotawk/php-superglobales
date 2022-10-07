2f4a7e2d1966c760c42146937f1b6af31d4b3e23
Jotawk <Jonathan.tawk@live.fr>
2022-10-07 09:28:41 +0200

    Ajout tailwind css recap.php

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Exercice superglobales</title>
+	<!-- <script src="https://cdn.tailwindcss.com"></script> -->
 </head>
 
 <body>
-	<h1>Ajouter un produit</h1>
+	<h1 class="text-4xl">Ajouter un produit</h1>
 	<form action="traitement.php" method="post">
 		<p>
 			<label>
@@ -25,7 +26,7 @@
 		</p>
 		<p>
 			<label>
-				Quantité désirée:
+				Quantité désirée :
 				<input type="number" name="qtt" value="1">
 			</label>
 		</p>
--- recap.php
+++ recap.php
@@ -2,13 +2,14 @@
 session_start();
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Récapitulatif des produits</title>
+	<script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
@@ -17,38 +18,42 @@ session_start();
 		echo "<p>Aucun produit en session...</p>";
 	} else {
 		echo
-			"<table>",
-				"<thead>",
+			"<div class='grid place-items-center h-screen'>",
+				"<div>",
+				"<table class='table-auto border-spacing-3 border border-slate-400 text-center'>",
+					"<thead>",
+						"<tr>",
+							"<th class='border-slate-300'>#</th>",
+							"<th class='border border-slate-300 bg-slate-300 p-3'>Nom</th>",
+							"<th class='border border-slate-300 bg-slate-300 p-3'>Prix</th>",
+							"<th class='border border-slate-300 bg-slate-300 p-3'>Quantité</th>",
+							"<th class='border border-slate-300 bg-slate-300 p-3'>Total</th>",
+						"</tr>",
+					"</thead>",
+					"<tbody>",
+				"</div>";
+
+			$totalGeneral = 0;
+
+			foreach ($_SESSION['products'] as $index => $product) {
+				echo
 					"<tr>",
-						"<th>#</th>",
-						"<th>Nom</th>",
-						"<th>Prix</th>",
-						"<th>Quantité</th>",
-						"<th>Total</th>",
+						"<td class='border-slate-300 p-4'>".$index."</td>",
+						"<td class='border border-slate-300'>".$product['name']."</td>",	
+						"<td class='border border-slate-300'>". number_format($product['price'], 2, ",", "&nbsp;") . "&nbsp;€</td>",	
+						"<td class='border border-slate-300'>".$product['qtt']."</td>",	
+						"<td class='border border-slate-300'>". number_format($product['total'], 2, ",", "&nbsp;") . "&nbsp;€</td>",
+					"</tr>";	
+				$totalGeneral += $product['total'];		
+				}
+				echo
+					"<tr>",
+						"<td colspan=4 class='border border-slate-300 bg-slate-500'>Total général : </td>",
+						"<td><strong class='border border-slate-300'>". number_format($totalGeneral, 2, ",", "&nbsp;") . "&nbsp;€</td>",
 					"</tr>",
-				"</thead>",
-				"<tbody>";
-
-		$totalGeneral = 0;
-
-		foreach ($_SESSION['products'] as $index => $product) {
-			echo
-				"<tr>",
-					"<td>".$index."</td>",
-					"<td>".$product['name']."</td>",	
-					"<td>". number_format($product['price'], 2, ",", "&nbsp;") . "&nbsp;€</td>",	
-					"<td>".$product['qtt']."</td>",	
-					"<td>". number_format($product['total'], 2, ",", "&nbsp;") . "&nbsp;€</td>",
-				"</tr>";
-			$totalGeneral += $product['total'];		
-			}
-			echo
-				"<tr>",
-					"<td colspan=4>Total général : </td>",
-					"<td><strong>". number_format($totalGeneral, 2, ",", "&nbsp;") . "&nbsp;€</td>",
-				"</tr>",
-				"</tbody>",
-			"</table>";
+					"</tbody>",
+				"</table>",
+			"</div>";
 		}
 
 	?>
