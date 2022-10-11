9b4dc71c5c65e61caee0e24658aea0ca0654d06c
Jonathan Tawk <Jonathan.Tawk@live.fr>
2022-10-11 11:40:06 +0200

    testcommit

--- index.php
+++ index.php
@@ -17,17 +17,22 @@ session_start();
 
 	<?php 
 	
-if (isset($_SESSION['success'])) {
-	echo "
-		<div class='info-message p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800'>
-		<p>Votre produit a été ajouté avec succès</p>
-		</div>";
-} else if (isset($_SESSION['error'])) {
-	echo "
-		<div class='info-message p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800'>
-		<p>Il semblerait qu'il y ait une erreur. Merci de remplir correctement le formulaire</p>
-		</div>";
-} 
+	if (isset($_SESSION['success'])) {
+		$product = $_SESSION["products"];
+		$lastProduct = $product[array_key_last($product)]["name"];
+		
+		echo "
+			<div class='info-message p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800'>
+			<p>Votre produit " . $lastProduct . " a été ajouté avec succès</p>
+			</div>";
+
+
+	} else if (isset($_SESSION['error'])) {
+		echo "
+			<div class='info-message p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800'>
+			<p>Il semblerait qu'il y ait une erreur. Merci de remplir correctement le formulaire</p>
+			</div>";
+	} 
 
 	?>
 	
--- recap.php
+++ recap.php
@@ -5,7 +5,6 @@ unset($_SESSION['error']);
 ?>
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -14,11 +13,11 @@ unset($_SESSION['error']);
 	<script src="https://cdn.tailwindcss.com"></script>
 	<script src="https://cdn.lordicon.com/pzdvqjsp.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-	
 </head>
 
 <body>
 	<?php
+	require 'menu.php';
 	require 'functions.php';
 	echo showMessage();
 	
